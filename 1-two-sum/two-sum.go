e450fa1303f35fd30212b09df67f972a50fd2836
Brendan Devenney <brendan@devenney.io>
2025-07-24 12:21:43 +0100

    Time: 0 ms (100.00%) | Memory: 6 MB (18.97%) - LeetSync

---
+++ 1-two-sum/two-sum.go
@@ -0,0 +1,15 @@
+// @param nums: array of integers, can be negative
+// @param target: sum target, can be negative
+func twoSum(nums []int, target int) []int {
+    seen := make(map[int]int)
+    
+    for i, val := range nums {
+        j, ok := seen[target - val]
+        if ok {
+            return []int{j, i}
+        }
+        seen[val] = i
+    }
+
+    return nil
+}

62253db5ab5a974af33755b04c794e3d03186d2c
Brendan Devenney <brendan@devenney.io>
2025-07-24 12:22:33 +0100

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,15 @@
+# Last updated: 24/07/2025, 12:22:33
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # track num: index
+        seen = {}
+        for i, num in enumerate(nums):
+            # calculate diff between our number and the target
+            diff = target - num
+
+            # if we've seen this diff as a previous number, we have solved
+            if diff in seen:
+                return [seen[diff], i]
+
+            # otherwise just track this number (key) and its index (value)
+            seen[num] = i
